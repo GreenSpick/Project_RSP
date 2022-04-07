5e492349aa2ae3027efdb1c315e2f02386177585
JojoPohuist <payebolnikov@mail.ru>
2022-04-07 20:47:36 +0900

    Edit interface

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ def cleaning():
 def comparison(a, b):
     global wins, defeats, round
     if a == b:
-        print("\n", "                                No winner")
+        print("\n\n\n\n\n\n\n\n", "                                No winner")
     elif (a == 1 and b == 2) or (a == 3 and b == 1) or (a == 2 and b == 3):
-        print("\n", "                                You win")
+        print("\n\n\n\n\n\n\n\n", "                                You win")
         wins += 1
         round += 1
     elif (a == 1 and b == 3) or (a == 2 and b == 1) or (a == 3 and b == 2):
-        print("\n", "                                You defeat")
+        print("\n\n\n\n\n\n\n\n", "                                You defeat")
         defeats += 1
         round += 1
 
@@ -31,13 +31,13 @@ while True:
     PlayerChoiseInt = int(input("Stone = 1; Scissors = 2; Paper = 3 : "))
     OtherChoiseInt = random.randint(1, 3)
     comparison(PlayerChoiseInt, OtherChoiseInt)
-    print("(rounds): ", round, " (wins): ", wins, " (defeats): ", defeats, " (Player choise): ", IntToString(PlayerChoiseInt), " (Other choise): ", IntToString(OtherChoiseInt), "\n")
+    print("(rounds): ", round, " (wins): ", wins, " (defeats): ", defeats, " (Player choise): ", IntToString(PlayerChoiseInt), " (Other choise): ", IntToString(OtherChoiseInt), "\n\n\n")
 
     if round == 3 and wins > defeats:
-        print("            ---------------You have won this game!---------------\n")
+        print("\n\n\n\n\n\n\n\n\n\n            ---------------You have won this game!---------------\n")
         cleaning()
         continue
     elif round == 3 and wins < defeats:
-        print("            ---------------You lost in this game!---------------\n")
+        print("\n\n\n\n\n\n\n\n\n\n            ---------------You lost in this game!---------------\n")
         cleaning()
         continue
