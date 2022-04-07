55636e3b41d482a6b43d3e005a364cbd7c02e6a7
JojoPohuist <payebolnikov@mail.ru>
2022-04-07 23:25:48 +0900

    small changes

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 import random
 
-def cleaning():
-    global wins, defeats, round
+def Clean():
+    global wins, defeats, rounds
     wins = 0
     defeats = 0
-    round = 0
+    rounds = 0
 
-def comparison(a, b):
-    global wins, defeats, round
+def Compare(a, b):
+    global wins, defeats, rounds
     if a == b:
         print("\n" * 10, "                                No winner")
     elif (a == 1 and b == 2) or (a == 3 and b == 1) or (a == 2 and b == 3):
         print("\n" * 10, "                                You win")
         wins += 1
-        round += 1
+        rounds += 1
     elif (a == 1 and b == 3) or (a == 2 and b == 1) or (a == 3 and b == 2):
         print("\n" * 10, "                                You lose")
         defeats += 1
-        round += 1
+        rounds += 1
 
 def IntToString(a):
     return option[a]
@@ -25,22 +25,22 @@ def IntToString(a):
 option = {1 : "Rock", 2 : "Scissors", 3 : "Paper"}
 wins = 0
 defeats = 0
-round = 0
+rounds = 0
 
 while True:
     PlayerChoiseInt = int(input("Rock = 1; Scissors = 2; Paper = 3 : "))
     OtherChoiseInt = random.randint(1, 3)
-    comparison(PlayerChoiseInt, OtherChoiseInt)
+    Compare(PlayerChoiseInt, OtherChoiseInt)
     try:
-        print("(rounds): ", round, " (wins): ", wins, " (defeats): ", defeats, " (Player choise): ", IntToString(PlayerChoiseInt), " (Other choise): ", IntToString(OtherChoiseInt), "\n\n\n")
+        print("(rounds): ", rounds, " (wins): ", wins, " (defeats): ", defeats, " (Player choise): ", IntToString(PlayerChoiseInt), " (Other choise): ", IntToString(OtherChoiseInt), "\n\n\n")
     except BaseException:
         print("\n" * 15, "Try again pliz")
         continue
-    if round == 3 and wins > defeats:
+    if rounds == 3 and wins > defeats:
         print("\n" * 10 ,"            ---------------You have won this game!---------------\n")
-        cleaning()
+        Clean()
         continue
-    elif round == 3 and wins < defeats:
+    elif rounds == 3 and wins < defeats:
         print("\n" * 10 ,"            ---------------You lost in this game!---------------\n")
-        cleaning()
+        Clean()
         continue
